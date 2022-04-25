c21fdec722ceb3369e91e9bca9307bad3aea8d0a
taquero-programador <jrangel93@protonmail.com>
2022-04-25 17:55:44 -0500

    feat(main): init
    anotaciones de funciones

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-	return {'mensaje': 'hello world!!'}
+#@app.get('/')
+#async def root():
+#    return {'mensaje': 'hello world!!'}
+
+# usar el metodo string format
+#@app.get('/items/{item_id}')
+#async def read_item(item_id):
+#	return {'item_id': item_id}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
