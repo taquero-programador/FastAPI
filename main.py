a6ec355226df44902d939d3f0c97f734796f7402
taquero-programador <jrangel93@protonmail.com>
2022-04-25 21:27:24 -0500

    feat(main): Enum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
+from enum import Enum
+
 
 app = FastAPI()
 
@@ -17,10 +19,28 @@ app = FastAPI()
 #async def read_item(item_id: int):
 #    return {"item_id": item_id}
 
-@app.get('/users/me')
-async def read_user_me():
-    return {'user_id': 'actual user'}
+#@app.get('/users/me')
+#async def read_user_me():
+#    return {'user_id': 'actual user'}
 
-@app.get('/users/{user_id}')
-async def read_user(user_id: str):
-    return {'user_id': user_id}
+#@app.get('/users/{user_id}')
+#async def read_user(user_id: str):
+#    return {'user_id': user_id}
+
+class ModelName(str, Enum):
+
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
+
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+
+    if model_name == ModelName.alexnet:
+        return {'model_name': model_name, 'message': 'Deep learning FTW!'}
+
+    if model_name.value == 'lenet':
+        return {'model_name': model_name, 'message': 'LeCNN'}
+
+
+    return {'model_name': model_name, 'message': 'residuals'}
